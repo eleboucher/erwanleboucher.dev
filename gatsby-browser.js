ef409b8b1003cfc8a1e89f06a8ce00bedf895968
Erwan Leboucher <erwanleboucher@gmail.com>
2019-02-03 20:25:48 +0100

    add service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
