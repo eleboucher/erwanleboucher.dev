a0752aff66212b9aba57e93d21b7bbf2b37ee842
Erwan Leboucher <erwanleboucher@gmail.com>
2019-02-03 22:16:19 +0100

    improve footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { Text } from './typography'
+
+import Github from '../images/github-logo-black.svg'
 
 const Wrapper = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-content: center;
+`
+
+const Icon = styled.img`
+  height: 50px;
+  margin-left: 5px;
+`
+
+const Item = styled.div`
+  display: flex;
+  align-items: center;
 `
 
 const Footer = () => (
   <Wrapper>
-    <Text>® {new Date().getFullYear()} Erwan Leboucher</Text>
+    <Item>
+      <Text>Find the Code on Github</Text>
+      <a href="https://github.com/genesixx/erwanleboucher.fr">
+        <Icon src={Github} />
+      </a>
+    </Item>
+    <Item>
+      <Text>® {new Date().getFullYear()} Erwan Leboucher</Text>
+    </Item>
   </Wrapper>
 )
 
