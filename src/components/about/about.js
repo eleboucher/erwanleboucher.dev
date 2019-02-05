95eda6b49c5f23dd488b855d34d2ca79a0699e62
Erwan Leboucher <erwanleboucher@gmail.com>
2019-02-06 00:03:31 +0100

    set max width to about me text

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Pannel from '../Pannel'
+import styled from 'styled-components'
 import { Text } from '../typography'
 
 const calculateAge = () => {
@@ -10,15 +11,22 @@ const calculateAge = () => {
   return Math.abs(ageDate.getUTCFullYear() - 1970)
 }
 
+const Wrapper = styled.div`
+  max-width: 700px;
+  align-self: center;
+`
+
 const About = () => (
   <Pannel title="About Me">
-    <Text>
-      Hi,
-      <br /> I'm a {calculateAge()} year old Full-Stack developer living in
-      Paris. Enthusiast about anything related to new technology, science or
-      engineering. I'm as well interested by cars, especially Formula 1, Sci-fi
-      and musics.
-    </Text>
+    <Wrapper>
+      <Text>
+        Hi,
+        <br /> I'm a {calculateAge()} year old Full-Stack developer living in
+        Paris. Enthusiast about anything related to new technology, science or
+        engineering. I'm as well interested by cars, especially Formula 1,
+        Sci-fi and musics.
+      </Text>
+    </Wrapper>
   </Pannel>
 )
 
