eff541816f6b729b3a1d7c4bc5e8ed52ef812c56
Erwan Leboucher <erwan.leboucher@gitguardian.com>
2019-02-06 10:37:41 +0100

    don't ask to reload the page when new version is available

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,14 +5,3 @@
  */
 
 // You can delete this file if you're not using it
-
-exports.onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`
-  )
-
-  if (answer === true) {
-    window.location.reload()
-  }
-}
