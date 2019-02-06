578e0473f71273f5f81b47387ce5a5bd7aeec705
Erwan Leboucher <erwan.leboucher@gitguardian.com>
2019-02-06 10:23:46 +0100

    change resume render

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -4,9 +4,28 @@ import RawPannel from '../Pannel'
 import { StaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
 import Download from '../../images/download.svg'
+import { Text as RawText } from '../typography'
 
 const Pannel = styled(RawPannel)`
   min-height: 75vh;
+`
+const Text = styled(RawText)`
+  color: var(--white);
+  font-weight: bold;
+`
+const StyledLink = styled.a`
+  align-self: center;
+  text-decoration: none;
+  text-align: center;
+  transition: opacity 0.2s linear;
+
+  &:hover {
+    opacity: 0.7;
+  }
+`
+
+const Img = styled.img`
+  height: 100px;
 `
 
 const About = () => (
@@ -20,9 +39,14 @@ const About = () => (
     `}
     render={({ file }) => (
       <Pannel title="Download my resume" primary>
-        <a href={file.publicURL} target="_blank" rel="noopener noreferrer">
-          <img src={Download} height="100px" alt="download my resume" />
-        </a>
+        <StyledLink
+          href={file.publicURL}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Img src={Download} alt="download my resume" />
+          <Text>Download Here</Text>
+        </StyledLink>
       </Pannel>
     )}
   />
