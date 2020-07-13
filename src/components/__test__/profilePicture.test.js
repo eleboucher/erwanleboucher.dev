1e84e7e72e14f60f7ed42171b03a0fbc589c714b
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2020-07-13 15:43:37 +0200

    Add missing snapshot on component

---
+++ src/components/__test__/profilePicture.test.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import "jest-styled-components"
+
+import ProfilePicture from "../profilePicture"
+
+describe("ProfilePicture", () => {
+  it("renders correctly", () => {
+    const { container } = render(<ProfilePicture />)
+    expect(container.innerHTML).toMatchSnapshot()
+  })
+})
