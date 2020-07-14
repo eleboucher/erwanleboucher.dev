9cdac61baef505aa34e2c74165876d98139a6cf8
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2020-07-14 08:47:39 +0200

    Add animation on the divider

--- src/components/divider.tsx
+++ src/components/divider.tsx
@@ -6,6 +6,15 @@ const Divider = styled.div`
   height: 3px;
   background-color: var(--dark);
   margin: 10px 0;
+  animation: reveal 0.7s forwards;
+  @keyframes reveal {
+    from {
+      clip-path: inset(0 0 0 100%);
+    }
+    to {
+      clip-path: inset(0 0 0 0);
+    }
+  }
 `
 
 export default Divider
