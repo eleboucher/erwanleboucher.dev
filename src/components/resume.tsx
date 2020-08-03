3eb8cf67bc9677d7ee0b3da58ba76c594db218e9
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2020-08-03 15:50:26 +0200

    put resume as static file

--- src/components/resume.tsx
+++ src/components/resume.tsx
@@ -1,31 +1,19 @@
 import React from "react"
-import styled from "styled-components"
-import { useStaticQuery, graphql } from "gatsby"
 import { StyledLink } from "./socials"
 
 import File from "../images/file.svg"
 
-const Resume = ({ size }) => {
-  const resume = useStaticQuery(graphql`
-    {
-      file(name: { eq: "resume" }) {
-        publicURL
-      }
-    }
-  `)
-
-  return (
-    <StyledLink
-      href={resume.file.publicURL}
-      target="_blank"
-      rel="noopener noreferrer"
-      aria-label="resume"
-      size={size}
-      style={{ marginLeft: 10 }}
-    >
-      <File alt="resume" loading="lazy" />
-    </StyledLink>
-  )
-}
+const Resume = ({ size }) => (
+  <StyledLink
+    href="/resume.pdf"
+    target="_blank"
+    rel="noopener noreferrer"
+    aria-label="resume"
+    size={size}
+    style={{ marginLeft: 10 }}
+  >
+    <File alt="resume" loading="lazy" />
+  </StyledLink>
+)
 
 export default Resume
