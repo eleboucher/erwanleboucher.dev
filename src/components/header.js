8b3594f699d242792b4411e92e35586b83ae40af
Erwan Leboucher <erwan.leboucher@barzahlen.de>
2021-04-22 17:05:41 +0200

    fix seo

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,25 @@
 import React from "react"
-import { Link as RawLink } from "gatsby"
-
-const Link = ({ children, className, ...props }) => (
-  <RawLink className={"mr-3 font-title " + className} {...props}>
-    {children}
-  </RawLink>
-)
+import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div className="flex p-3 flex-col justify-center items-center md:justify-between md:flex-row">
-      <Link to="/" className="text-4xl">
+    <div className="flex p-3 flex-col justify-center items-center md:justify-between md:flex-row ">
+      <Link to="/" className="text-4xl font-title">
         {siteTitle}
       </Link>
-      <div className="flex justify-center items-center pr-3 md:justify-between">
-        <Link to="/">About</Link>
-        <Link to="/journey">Journey</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/blog">Blog</Link>
+      <div className="flex justify-center items-center pr-3 md:justify-between flex-wrap mt-5 md:mt-0">
+        <Link to="/" className="text-xl mr-4 font-title">
+          About
+        </Link>
+        <Link to="/journey" className="text-xl mr-4 font-title">
+          Journey
+        </Link>
+        <Link to="/projects" className="text-xl mr-4 font-title">
+          Projects
+        </Link>
+        <Link to="/blog" className="text-xl font-title">
+          Blog
+        </Link>
       </div>
     </div>
   </header>
--- src/components/profilePicture.js
+++ src/components/profilePicture.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 const ProfilePicture = () => {
