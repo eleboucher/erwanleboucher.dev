fe58b22ce296decdf6cc5219729833b78942fefe
Erwan Leboucher <erwanleboucher@gmail.com>
2026-03-23 20:44:21 +0100

    fix build

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="vite/client" />
 
+declare module '@fontsource-variable/atkinson-hyperlegible-mono'
+
 declare module 'virtual:posts-meta' {
   const posts: { slug: string; title: string; date: string; description: string }[]
   export default posts
